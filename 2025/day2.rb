bb3ff358094c42fd80f0501f0d41b4a65e70716b
Alan Marx <alanmarx@Alans-MacBook-Air.local>
2025-12-02 17:37:26 +0100

    solve day2

---
+++ 2025/day2.rb
@@ -0,0 +1,30 @@
+require 'debug'
+
+path = File.join(File.dirname(__FILE__), 'day2.txt')
+
+ranges = File.read(path).split(',').map do |range|
+  lower, upper = range.split('-').map(&:to_i)
+
+  Range.new(lower, upper)
+end
+
+def count?(string)
+  mid = string.length / 2
+
+  chars = string.chars 
+
+  mid.downto(1) do |i| 
+    return true if chars.each_slice(i).uniq.one?
+  end
+
+  false
+end
+
+total = ranges.sum do |range|
+  range.sum do |number|
+    count?(number.to_s) ? number : 0 
+  end
+end
+
+puts total
+
