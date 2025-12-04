b8f129f569d4c8f1cd569bd194812b60ae09b722
Alan Marx <alanmarx@Alans-MacBook-Air.local>
2025-12-04 22:17:36 +0100

    solve part 2

--- 2025/day4.rb
+++ 2025/day4.rb
@@ -1,36 +1,62 @@
 require 'debug'
 
-grid = File.readlines(File.join(__dir__, 'day4.txt'), chomp: true).collect(&:chars)
+def extract_paper_rolls(grid)
+  paper_rolls = {}
 
-paper_roll_neighbour_count = {}
-
-grid.each_with_index do |row, row_number|
-  row.each_with_index do |cell, column_number|
-    paper_roll_neighbour_count[[row_number, column_number]] = 0 if cell == '@'
+  grid.each_with_index do |row, row_number|
+    row.each_with_index do |cell, column_number|
+      paper_rolls[[row_number, column_number]] = 0 if cell == '@'
+    end
   end
+
+  paper_rolls
 end
 
-grid.each_with_index do |row, row_number|
-  row.each_with_index do |cell, column_number|
-    next if cell != '@'
-
-    [ 
-      # row above
-      [row_number - 1, column_number],
-      [row_number - 1, column_number - 1],
-      [row_number - 1, column_number + 1],
-       # same row
-      [row_number, column_number - 1],
-      [row_number, column_number + 1],
-      # row below
-      [row_number + 1, column_number],
-      [row_number + 1, column_number - 1],
-      [row_number + 1, column_number + 1],
-    ].each do |paper_roll|
-      paper_roll_neighbour_count[paper_roll] += 1 if paper_roll_neighbour_count.key?(paper_roll)
+def add_neighbours_to_paper_rolls(paper_rolls, grid)
+  grid.each_with_index do |row, row_number|
+    row.each_with_index do |cell, column_number|
+      next if cell != '@'
+  
+      [ 
+        # row above
+        [row_number - 1, column_number],
+        [row_number - 1, column_number - 1],
+        [row_number - 1, column_number + 1],
+         # same row
+        [row_number, column_number - 1],
+        [row_number, column_number + 1],
+        # row below
+        [row_number + 1, column_number],
+        [row_number + 1, column_number - 1],
+        [row_number + 1, column_number + 1],
+      ].each do |paper_roll|
+        paper_rolls[paper_roll] += 1 if paper_rolls.key?(paper_roll)
+      end
     end
   end
 end
 
-puts paper_roll_neighbour_count.count { |_paper_roll_position, neighbours| neighbours < 4 }
+def remove_paper_rolls(paper_rolls, grid)
+  paper_rolls.each do |paper_roll, _neighbours|
+    grid[paper_roll[0]][paper_roll[1]] = '.'
+  end
+
+  grid
+end
+
+def count_movable_paper_rolls(grid)
+  paper_rolls = extract_paper_rolls(grid)
+
+  add_neighbours_to_paper_rolls(paper_rolls, grid)
+
+  countable_paper_rolls = paper_rolls.select { |_paper_roll, neighbours| neighbours < 4 }
+
+  return 0 if countable_paper_rolls.empty?
+
+  countable_paper_rolls.count + count_movable_paper_rolls(remove_paper_rolls(countable_paper_rolls, grid))
+end
+
+grid = File.readlines(File.join(__dir__, 'day4.txt'), chomp: true).collect(&:chars)
+
+puts count_movable_paper_rolls(grid)
 
